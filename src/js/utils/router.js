d9ff8b5ad9b903390b7692a4257f03336897cbee
tsilva <tsilva@uplexis.com.br>
2020-03-18 11:02:56 -0300

    feature(routers): novos paths de rotas

--- src/js/utils/router.js
+++ src/js/utils/router.js
@@ -1,18 +1,33 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
 import Welcome from "../components/pages/Welcome.vue"
+import Aplications from "../components/pages/Aplications.vue"
+import Fonts from "../components/pages/Fonts.vue"
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 Vue.use(VueRouter)
+
+export const routes = [
+  {
+    path: "/",
+    name: 'Home',
+    component: Welcome,
+  },
+  {
+    path: "/aplicativos",
+    name: 'Aplicativos',
+    component: Aplications,
+  },
+  {
+    path: "/fontes",
+    name: 'Fontes',
+    component: Fonts,
+  },
+]
 
 const router = new VueRouter({
   history: true,
-  routes: [
-    {
-      path: "/",
-      component: Welcome,
-    },
-  ]
+  routes,
 })
 
 export default router
