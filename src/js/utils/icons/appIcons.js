d959c5baebb8298dd9ff44d174adf0bb50100b08
tsilva <tsilva@uplexis.com.br>
2020-03-19 14:26:37 -0300

    feature(icons): adicionando e mapeando icones

---
+++ src/js/utils/icons/appIcons.js
@@ -0,0 +1,16 @@
+export const icons = () => {
+  return [
+    { id: 0, path: '../../../assets/icons/apps/app_00.svg' },
+    { id: 1, path: '../../../assets/icons/apps/app_01.svg' },
+    { id: 2, path: '../../../assets/icons/apps/app_02.svg' },
+    { id: 3, path: '../../../assets/icons/apps/app_03.svg' },
+    { id: 4, path: '../../../assets/icons/apps/app_04.svg' },
+    { id: 5, path: '../../../assets/icons/apps/app_05.svg' },
+    { id: 6, path: '../../../assets/icons/apps/app_06.svg' },
+    { id: 7, path: '../../../assets/icons/apps/app_07.svg' },
+    { id: 8, path: '../../../assets/icons/apps/app_08.svg' },
+    { id: 9, path: '../../../assets/icons/apps/app_09.svg' },
+    { id: 10, path: '../../../assets/icons/apps/app_10.svg' },
+    { id: 11, path: '../../../assets/icons/apps/app_11.svg' },
+  ]
+}
---
+++ src/js/utils/icons/sourceIcons.js
@@ -0,0 +1,13 @@
+export const icons = () => {
+  return [
+    { id: 0, path: '../../../assets/icons/sources/source_00.svg' },
+    { id: 1, path: '../../../assets/icons/sources/source_01.svg' },
+    { id: 2, path: '../../../assets/icons/sources/source_02.svg' },
+    { id: 3, path: '../../../assets/icons/sources/source_03.svg' },
+    { id: 4, path: '../../../assets/icons/sources/source_04.svg' },
+    { id: 5, path: '../../../assets/icons/sources/source_05.svg' },
+    { id: 6, path: '../../../assets/icons/sources/source_06.svg' },
+    { id: 7, path: '../../../assets/icons/sources/source_07.svg' },
+    { id: 8, path: '../../../assets/icons/sources/source_08.svg' },
+  ]
+}
